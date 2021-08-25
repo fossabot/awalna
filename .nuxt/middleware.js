d0b20d72643beb1af4197e24e633f9a2226a5a33
madjidsmail <madjidsmail1998@gmail.com>
2021-08-25 01:04:36 +0100

    all or nothing

--- .nuxt/middleware.js
+++
@@ -1,9 +0,0 @@
-const middleware = {}
-
-middleware['isAdmin'] = require('../middleware/isAdmin.js')
-middleware['isAdmin'] = middleware['isAdmin'].default || middleware['isAdmin']
-
-middleware['isAuthenticated'] = require('../middleware/isAuthenticated.js')
-middleware['isAuthenticated'] = middleware['isAuthenticated'].default || middleware['isAuthenticated']
-
-export default middleware
